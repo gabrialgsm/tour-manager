07982ed9a4d5311dd44a495d7a1a2a65aca5d63b
Gabrial Rozario <gabrial.gsm@gmail.com>
2026-09-15 04:26:49 +0600

    Add Facebook passenger auth endpoint placeholder

---
+++ passenger_facebook_auth.php
@@ -0,0 +1,5 @@
+<?php
+// Facebook passenger OAuth endpoint placeholder; configuration and identity linking will be added in the auth hardening pass.
+require_once __DIR__.'/bootstrap_saas.php';
+http_response_code(501);
+exit('Facebook login is not configured yet.');
