e63e8299f7b90d2cedffb3c23bf18e7946c7d419
Gabrial Rozario <gabrial.gsm@gmail.com>
2026-09-18 17:10:54 +0600

    brand: rename application session

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 $config=require __DIR__.'/config.php';
 date_default_timezone_set($config['app']['timezone']??'Asia/Dhaka'); 
-session_name('GMJSSESSID');
+session_name('GOTMSESSID');
 session_set_cookie_params(['httponly'=>true,'secure'=>(!empty($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!=='off'),'samesite'=>'Lax']);
 session_start();
 function db():PDO{static $p;global $config;if(!$p){$d=$config['db'];$p=new PDO("mysql:host={$d['host']};dbname={$d['name']};charset={$d['charset']}",$d['user'],$d['pass'],[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_EMULATE_PREPARES=>false]);}return $p;}
